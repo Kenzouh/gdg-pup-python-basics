f03e2279d39b95e37c80dde309db7f6c67cdd7b1
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-03-01 17:14:42 +0800

    feat: complete Python assignment 1

--- Assignments-Exercises/Python-1_Introduction-to-Syntax/main.py
+++ Assignments-Exercises/Python-1_Introduction-to-Syntax/main.py
@@ -1,25 +1,39 @@
 # This program explores Python syntax, variables, data types, and basic operators.
 
 # 1. Declare variables of different types
-name = "Sparky"  # string
-age = 25        # integer
-height = 5.6     # float
+name = "Kenzo"  # string
+
+age = 22        # integer
+year_level = 3
+
+weight = 155.5     # float
+gwa = 1.18
+
 is_student = True  # boolean
+is_cute = True 
 
 # 2. Perform basic arithmetic operations
-sum_result = age + height
-product_result = age * height
+sum_result = age + weight
+product_result = age * weight
 
 # 3. Concatenate strings and display outputs
 greeting = "Hello, " + name + "!"
 print(greeting)
 
 # 4. Display results of arithmetic operations
-print("Sum of age and height:", sum_result)
-print("Product of age and height:", product_result)
+print("\nSum of age and height:", sum_result)
+print("Product of age and height:", product_result, "\n")
 
 # 5. Display the types of each variable
 print("Type of name:", type(name))
+
 print("Type of age:", type(age))
-print("Type of height:", type(height))
+print("Type of year_level:", type(year_level), "\n")
+
+print("Type of gwa:", type(gwa))
+print("Type of height:", type(weight))
+
 print("Type of is_student:", type(is_student))
+print("Type of is_cute:", type(is_cute))
+
+
